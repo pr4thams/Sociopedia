4a1bf401aaf08df449a438eb1166ff5649558021
Pratham Sharma <sharma.pratham0502@gmail.com>
2023-05-13 19:55:24 -0230

    fixed post url fetching

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -64,7 +64,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`https://sociopedia-pr4thams.herokuapp.com/${picturePath}`}
+          src={`https://sociopedia-pr4thams.herokuapp.com/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
